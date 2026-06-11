05ac6256a7dab96f9a22f645f8b122ac26d6d7dd
NariChiiga <buza1058@gmail.com>
2026-06-11 11:19:22 +0300

    refactor: address review comments

--- src/task.js
+++ src/task.js
@@ -14,6 +14,13 @@ class Task {
             this.status = status;
         }
     }
+	this.labels = [];
+
+addLabel(label) {
+    if (label && !this.labels.includes(label)) {
+        this.labels.push(label);
+    }
+ }
 }
 
 module.exports = Task;
